e1d12844e4f5d8a7459a0b1a60f4f5133a41e39f
Joseph Mignone <jmig3066@gmail.com>
2021-07-27 21:32:58 -0400

    Delete Run_Win11-TPM-RegBypass.bat

--- Run_Win11-TPM-RegBypass.bat
+++
@@ -1,4 +0,0 @@
-SET /P win11iso=Windows 11 ISO Path: 
-SET /P dest_iso=Destination ISO: 
-
-powershell -noprofile -command "&{ start-process powershell -ArgumentList '-noprofile -file %~dp0\Win11-TPM-RegBypass.ps1 -Win11Image %win11iso% -DestinationImage %dest_iso%' -verb RunAs}"
