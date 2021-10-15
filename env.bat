e54fbe5e367d6e9fd531f824b1c00bebbf713bed
Joseph Mignone <jmig3066@gmail.com>
2021-10-15 13:41:44 -0400

    Update env.bat

--- env.bat
+++ env.bat
@@ -1,2 +1,3 @@
 @echo OFF
+:: Start a new instance of PowerShell requesting admin access, and set it to the directory of the repository (aka. the current directory)
 powershell Start-Process powershell.exe -verb runAs -ArgumentList '-NoExit', '-Command', 'cd %CD%'
