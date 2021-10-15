3c28f9b1a876fcc44998d6d18854f6372db977cd
Joseph Mignone <jmig3066@gmail.com>
2021-10-15 13:47:19 -0400

    Add comments

--- env.bat
+++ env.bat
@@ -1,3 +1,9 @@
-@echo OFF
+::env.bat
 :: Start a new instance of PowerShell requesting admin access, and set it to the directory of the repository (aka. the current directory)
+@echo OFF
+
+:: ----Breakdown----
+:: -verb runAs: request admin privileges
+:: -NoExit: prevent PowerShell instance from exiting after command is finished; this is important.
+:: -'cd %CD%: the command to run; sets the current path of the PowerShell instance to the current directory
 powershell Start-Process powershell.exe -verb runAs -ArgumentList '-NoExit', '-Command', 'cd %CD%'
