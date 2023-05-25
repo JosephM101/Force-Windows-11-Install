24080224c01d9f1c5ce268bd0e6a3cb67093930c
Joseph Mignone <jmig3066@gmail.com>
2023-05-25 12:47:56 -0400

    Add Pause command at very end

--- DiscardAllMountedImages.cmd
+++ DiscardAllMountedImages.cmd
@@ -4,4 +4,5 @@ $_Script = {
   Write-Host "Dismounting all mounted Windows images..."
   Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\WIMMount\Mounted Images" | Get-ItemProperty | Select -ExpandProperty "Mount Path" | ForEach-Object {Dismount-WindowsImage -Path $_ -Discard}
   Write-Host "Done."
+  Pause
 } ; start -verb runas powershell -args "-nop -c & {`n`n$($_Script-replace'"','\"')}"
